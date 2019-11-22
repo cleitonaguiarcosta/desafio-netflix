f8636c0970268ac0878e229189688e2a23e11922
vai na web <vainawebcpx@gmail.com>
2019-11-22 12:55:11 -0300

    aula quase finalizazada faltando o footer

--- main.js
+++ main.js
@@ -11,4 +11,43 @@ function search(){
      }
     
     }
+
+    function clicardireita1(){
+      document.querySelector('.block__series').style.display = 'none'
+      document.querySelector('.block__series--slide').style.display = 'flex'
+   }
    
+   function clicaresquerda1(){
+      document.querySelector('.block__series--slide').style.display = 'none'
+      document.querySelector('.block__series').style.display = 'flex'
+   }
+   
+   function clicardireita2(){
+      document.querySelector('.block__movies').style.display = 'none'
+      document.querySelector('.block__movies--slide').style.display = 'flex'
+   }
+   
+   function clicaresquerda2(){
+      document.querySelector('.block__movies--slide').style.display = 'none'
+      document.querySelector('.block__movies').style.display = 'flex'
+   }
+   
+   function clicardireita3(){
+      document.querySelector('.block__recently').style.display = 'none'
+      document.querySelector('.block__recently--slide').style.display = 'flex'
+   }
+   
+   function clicaresquerda3(){
+      document.querySelector('.block__recently--slide').style.display = 'none'
+      document.querySelector('.block__recently').style.display = 'flex'
+   }
+   
+  //  function clicardireita4(){
+  //     document.querySelector('.block__mylist').style.display = 'none'
+  //     document.querySelector('.block__mylist--slide').style.display = 'flex'
+  //  }
+   
+  //  function clicaresquerda4(){
+  //     document.querySelector('.block__mylist--slide').style.display = 'none'
+  //     document.querySelector('.block__mylist').style.display = 'flex'
+  //  }
